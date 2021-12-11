c98224c8aace32e62e7de1c3155e53380321fb85
fs5013-furi-sutao <fs5013.furi.sutao@google.com>
2021-12-12 02:25:32 +0900

    スマホでも見れるように CSS を調整

--- components/molecules/Layout.tsx
+++ components/molecules/Layout.tsx
@@ -4,14 +4,23 @@ import { Layout as AntdLayout } from 'antd';
 const Layout: React.FC = ({ children }) => (
   <>
     <AntdLayout style={{ minHeight: '100vh' }}>
-      <div style={{ padding: '2.5rem' }}>
+      <div>
+        {/*<div style={{ padding: '2.5rem' }}>*/}
         <div className="container">{children}</div>
       </div>
     </AntdLayout>
     <style jsx>{`
-      .container {
-        margin: 2.5rem 10rem;
+      @media (max-width: 560px) {
+        .container {
+          margin: 0rem 0rem;
+        }
       }
+
+      @media (min-width: 1025px) {
+        .container {
+          margin: 2.5rem 10rem;
+        }
+
     `}</style>
   </>
 );
